91032168e53ed4e7f73eaa32bca88985e832084d
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-02-07 22:19:23 +0100

    added pagination to edit post route

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -26,7 +26,7 @@ try {
       totalData,
       taskPerReq,
       totalPages,
-      dataToShow,
+      dataToShow, sorted
     });
   } else {
     let data = await TodoTask.find()
@@ -39,7 +39,7 @@ try {
     totalData,
     taskPerReq,
     totalPages,
-    dataToShow,
+    dataToShow, sorted
   });
   }
 
@@ -66,28 +66,63 @@ router.post("/", async (req, res) => {
 
 //UPDATE
 router.get("/edit/:id", async (req, res) => {
+  const page = +req.query.page || 1;
+  const totalData = await TodoTask.find().countDocuments();
+  const taskPerReq = 5;
+  const totalPages = Math.ceil(totalData / taskPerReq);
+  const dataToShow = taskPerReq * page;
+  const sorted = +req.query.sorted || 1;
   const id = req.params.id;
-  try {
-  await TodoTask.find({}, (err, tasks) => {
-    res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+  
+try {
+if (page === 1 ) {
+  let data = await TodoTask.find()
+    .limit(dataToShow)
+    .sort({  date: sorted })
+    console.log(data)
+  res.render("todoEdit.ejs", {
+    todoTasks: data, idTask: id,
+    page,
+    totalData,
+    taskPerReq,
+    totalPages,
+    dataToShow, sorted
   });
-} catch (err){
-  res.redirect("/")
-}
+} else {
+  let data = await TodoTask.find()
+  .limit(5).skip((page -1) *5 )
+  .sort({ date: sorted });
+  console.log(data)
+res.render("todoEdit.ejs", {
+  todoTasks: data, idTask: id, 
+  page,
+  totalData,
+  taskPerReq,
+  totalPages,
+  dataToShow, sorted
 });
+}
 
-router.post("/edit/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-  await TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
-    if (err) return res.send(500, err);
-    res.redirect("/");
-  });
 }catch(err) {
- res.redirect("/")
+   res.redirect("/")
 }
-});
+  });
+ 
 
+  router.post("/edit/:id", async (req, res) => {
+    const page = +req.query.page || 1;
+    const id = req.params.id;
+    try {
+    await TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect('/');
+    })
+  }catch(err) {
+   res.redirect("/")
+  }
+  });
+  
+  
 //DELETE
 router.get("/remove/:id", (req, res) => {
   const id = req.params.id;
